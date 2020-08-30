69286070a38b92a8bcb88137622fa07430bebfae
kansasdev <mobilne@live.com>
2020-08-30 20:27:23 +0200

    Add project files.

---
+++ MultiplicationTable/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MultiplicationTable.Models;
+
+namespace MultiplicationTable.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
