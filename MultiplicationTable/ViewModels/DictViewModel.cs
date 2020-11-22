455cf6004eaaa07e69ced059c9b42f42d3e2c38b
Michal <Michal@DESKTOP-TTIA4I6>
2020-11-22 17:46:39 +0100

    android , no pol locale

--- MultiplicationTable/ViewModels/DictViewModel.cs
+++ MultiplicationTable/ViewModels/DictViewModel.cs
@@ -340,7 +340,12 @@ namespace MultiplicationTable.ViewModels
                 ButtonEnabled = false;
                 IEnumerable<Locale> locals = TextToSpeech.GetLocalesAsync().GetAwaiter().GetResult();
                 Locale local = locals.FirstOrDefault(y => string.Equals(y.Country, "POL"));
-                var mySpeechOptions = new SpeechOptions
+                if(local==null)
+                {
+                    local = locals.FirstOrDefault(y => string.Equals(y.Country, "USA"));
+                }
+
+                    var mySpeechOptions = new SpeechOptions
                 {
                     Volume = 1,
                     Pitch = 0.2f,
